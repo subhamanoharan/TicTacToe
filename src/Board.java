96dfc551c502f0c9b66820f28d3b2d83ce6d4c10
Subha <subhamanoharanbe@gmail.com>
2016-06-03 18:57:44 +0530

    Need to fix failing tests for multiple moves

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,30 @@
 public class Board {
 
+    private int player, x, y;
+
+    private int[][] state;
+
+    public Board() {
+        state = new int[3][3];
+    }
+
     public boolean move(int player, int x, int y) {
-        if (x < 0 || y < 0 || x > 3 || y > 3) return false;
+        if (isInvalidPosition(x, y))
+            return false;
+        this.player = player;
+        this.x = player;
+        this.y = player;
         return true;
     }
+
+    public int playerAt(int x, int y) {
+        if(x != this.x || y != this.y || isInvalidPosition(x, y))
+            return -1;
+        return player;
+    }
+
+    private boolean isInvalidPosition(int x, int y) {
+        return x < 0 || y < 0 || x > 3 || y > 3;
+    }
+
 }
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -1,41 +1,90 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class BoardTest {
     @Test
-    public void shouldMovePlayerToPosition() throws Exception {
+    public void shouldMovePlayerToPosition() {
         Board board = new Board();
         int player = 0;
         assertTrue(board.move(player, 1, 2));
     }
 
     @Test
-    public void shouldNotMovePlayerToXPositionLessThan0() throws Exception {
+    public void shouldNotMovePlayerToXPositionLessThan0() {
         Board board = new Board();
         int player = 0;
         assertFalse(board.move(player, -1, 2));
     }
 
     @Test
-    public void shouldNotMovePlayerToYPositionLessThan0() throws Exception {
+    public void shouldNotMovePlayerToYPositionLessThan0() {
         Board board = new Board();
         int player = 0;
         assertFalse(board.move(player, 2, -6));
     }
 
     @Test
-    public void shouldNotMovePlayerToXPositionGreaterThan3() throws Exception {
+    public void shouldNotMovePlayerToXPositionGreaterThan3() {
         Board board = new Board();
         int player = 0;
         assertFalse(board.move(player, 4, 2));
     }
 
     @Test
-    public void shouldNotMovePlayerToYPositionGreaterThan3() throws Exception {
+    public void shouldNotMovePlayerToYPositionGreaterThan3() {
         Board board = new Board();
         int player = 0;
         assertFalse(board.move(player, 2, 7));
+    }
+
+    @Test
+    public void shouldReturnPlayerAtPosition(){
+        Board board = new Board();
+        int player = 1;
+
+        board.move(player, 1, 1);
+        assertEquals(board.playerAt(1, 1), player);
+    }
+
+    @Test
+    public void shouldReturnPlayerAtPositionIfXandYMatch(){
+        Board board = new Board();
+        int player = 1;
+
+        board.move(player, 1, 1);
+        assertEquals(board.playerAt(2, 1), -1);
+    }
+
+
+    @Test
+    public void shouldReturnNegativeIfThereIsNoPlayerAtPosition(){
+        Board board = new Board();
+        int player = 0;
+
+        board.move(player, 1, 1);
+        assertEquals(board.playerAt(0, 1), -1);
+    }
+
+    @Test
+    public void shouldReturnNegativeIfPositionForPlayerIsInvalid(){
+        Board board = new Board();
+        int player = 0;
+
+        board.move(player, 1, 1);
+        assertEquals(board.playerAt(-3, 1), -1);
+    }
+
+    @Test
+    public void shouldReturnPlayerAtPositionAfterMoreThanOneMove(){
+        Board board = new Board();
+        int player1 = 1;
+        int player2 = 0;
+
+        board.move(player1, 1, 1);
+        board.move(player2, 1, 2);
+        assertEquals(board.playerAt(1, 1), player1);
     }
 }

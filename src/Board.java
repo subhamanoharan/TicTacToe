7d3cc5db3778cb805a16014eb8b8c69e1261bcf3
Subha <subhamanoharanbe@gmail.com>
2016-06-03 19:14:24 +0530

    Introduce player enum

--- src/Board.java
+++ src/Board.java
@@ -1,30 +1,25 @@
 public class Board {
 
-    private int player, x, y;
-
-    private int[][] state;
+    private Player[][] state;
 
     public Board() {
-        state = new int[3][3];
+        state = new Player[3][3];
     }
 
-    public boolean move(int player, int x, int y) {
+    public boolean move(Player player, int x, int y) {
         if (isInvalidPosition(x, y))
             return false;
-        this.player = player;
-        this.x = player;
-        this.y = player;
+        state[x][y] = player;
         return true;
     }
 
-    public int playerAt(int x, int y) {
-        if(x != this.x || y != this.y || isInvalidPosition(x, y))
-            return -1;
-        return player;
+    public Player playerAt(int x, int y) {
+        if(isInvalidPosition(x, y))
+            return Player.INVALID;
+        return state[x][y];
     }
 
     private boolean isInvalidPosition(int x, int y) {
         return x < 0 || y < 0 || x > 3 || y > 3;
     }
-
 }
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -8,83 +8,64 @@ public class BoardTest {
     @Test
     public void shouldMovePlayerToPosition() {
         Board board = new Board();
-        int player = 0;
-        assertTrue(board.move(player, 1, 2));
+        assertTrue(board.move(Player.HUMAN, 1, 2));
     }
 
     @Test
     public void shouldNotMovePlayerToXPositionLessThan0() {
         Board board = new Board();
-        int player = 0;
-        assertFalse(board.move(player, -1, 2));
+        assertFalse(board.move(Player.HUMAN, -1, 2));
     }
 
     @Test
     public void shouldNotMovePlayerToYPositionLessThan0() {
         Board board = new Board();
-        int player = 0;
-        assertFalse(board.move(player, 2, -6));
+        assertFalse(board.move(Player.HUMAN, 2, -6));
     }
 
     @Test
     public void shouldNotMovePlayerToXPositionGreaterThan3() {
         Board board = new Board();
-        int player = 0;
-        assertFalse(board.move(player, 4, 2));
+        assertFalse(board.move(Player.HUMAN, 4, 2));
     }
 
     @Test
     public void shouldNotMovePlayerToYPositionGreaterThan3() {
         Board board = new Board();
-        int player = 0;
-        assertFalse(board.move(player, 2, 7));
+        assertFalse(board.move(Player.COMPUTER, 2, 7));
     }
 
     @Test
     public void shouldReturnPlayerAtPosition(){
         Board board = new Board();
-        int player = 1;
 
-        board.move(player, 1, 1);
-        assertEquals(board.playerAt(1, 1), player);
+        board.move(Player.COMPUTER, 1, 1);
+
+        assertEquals(board.playerAt(1, 1), Player.COMPUTER);
     }
-
-    @Test
-    public void shouldReturnPlayerAtPositionIfXandYMatch(){
-        Board board = new Board();
-        int player = 1;
-
-        board.move(player, 1, 1);
-        assertEquals(board.playerAt(2, 1), -1);
-    }
-
 
     @Test
     public void shouldReturnNegativeIfThereIsNoPlayerAtPosition(){
         Board board = new Board();
-        int player = 0;
 
-        board.move(player, 1, 1);
-        assertEquals(board.playerAt(0, 1), -1);
+        board.move(Player.HUMAN, 1, 1);
+        assertEquals(board.playerAt(0, 1), null);
     }
 
     @Test
     public void shouldReturnNegativeIfPositionForPlayerIsInvalid(){
         Board board = new Board();
-        int player = 0;
 
-        board.move(player, 1, 1);
-        assertEquals(board.playerAt(-3, 1), -1);
+        board.move(Player.HUMAN, 1, 1);
+        assertEquals(board.playerAt(-3, 1), Player.INVALID);
     }
 
     @Test
     public void shouldReturnPlayerAtPositionAfterMoreThanOneMove(){
         Board board = new Board();
-        int player1 = 1;
-        int player2 = 0;
 
-        board.move(player1, 1, 1);
-        board.move(player2, 1, 2);
-        assertEquals(board.playerAt(1, 1), player1);
+        board.move(Player.COMPUTER, 1, 1);
+        board.move(Player.HUMAN, 1, 2);
+        assertEquals(board.playerAt(1, 1), Player.COMPUTER);
     }
 }
